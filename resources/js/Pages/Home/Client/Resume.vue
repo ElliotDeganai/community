af3802955272e7ec3c29c4bbc4248bd71afd7d21
ElliotDeganai <34213433+ElliotDeganai@users.noreply.github.com>
2023-10-24 18:12:54 +0200

    resume with teams

--- resources/js/Pages/Home/Client/Resume.vue
+++ resources/js/Pages/Home/Client/Resume.vue
@@ -51,6 +51,51 @@
                         </div>
                     </div>
                 </div>
+                <div class="py-4 lg:py-8">
+                    <div class="w-full py-8 lg:py-24 z-10">
+                        <div class="title-home">Teams</div>
+                    </div>
+                    <div class="flex flex-wrap px-2 lg:px-32">
+                        <div class="w-1/3 relative">
+                            <div class="font-bold py-2 px-1 text-white bg-green-700 text-base lg:text-xl w-full text-center truncate">Team Paris</div>
+                        </div>
+                        <div class="w-1/3">
+                            <div class="font-bold py-2 px-1 text-white bg-gray-900 text-base lg:text-xl w-full text-center truncate">Team Lux</div>
+                            <div>
+                            </div>
+                        </div>
+                        <div class="w-1/3">
+                            <div class="font-bold py-2 px-1 text-white bg-red-700 text-base lg:text-xl w-full text-center truncate">Team Suisse</div>
+                            <div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap px-2 lg:px-32">
+                        <div class="w-1/3 px-2">
+                            <div class="relative">
+                                <div class="text-sm lg:text-base relative w-full text-center py-2" :key="user.id" v-for="user in team_paris">
+                                    {{user.name}}
+                                    <div class="px-1 lg:px-8 absolute top-0 right-0 py-2">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-1/3 px-2">
+                            <div>
+                                <div class="text-sm lg:text-base w-full text-center py-2" :key="user.id" v-for="user in team_lux">
+                                    {{user.name}}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-1/3 px-2">
+                            <div>
+                                <div class="text-sm lg:text-base w-full text-center py-2" :key="user.id" v-for="user in team_swiss">
+                                    {{user.name}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="py-8 px-2 lg:px-32">
                     <div>
                         <div class="title-home">Récapitulatif</div>
@@ -99,6 +144,9 @@ export default {
         coming() { return this.allUsers.filter(u => u.is_partying && u.has_answered)},
         notComing() { return this.allUsers.filter(u => !u.is_partying && u.has_answered)},
         noAnswers() { return this.allUsers.filter(u => !u.has_answered)},
+        team_lux() { return this.allUsers.filter(u => u.team == 'lux')},
+        team_paris() { return this.allUsers.filter(u => u.team == 'paris')},
+        team_swiss() { return this.allUsers.filter(u => u.team == 'swiss')},
     },
     created() {
     },
