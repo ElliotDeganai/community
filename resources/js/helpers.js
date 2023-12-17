b7d193b5a84071305830123fa1e9568ab4c31192
ElliotDeganai <34213433+ElliotDeganai@users.noreply.github.com>
2023-12-17 13:04:42 +0100

    test

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -22,66 +22,68 @@ const helpers = {
         return section.page_fields.filter(field => field.name === field_name)[0];
     },
     getValue(doc_value) {
-        if (doc_value !== undefined && doc_value.value_text !== null) {
-            return {
-                docValue: doc_value.value_text,
-                type: 'text'
-            };
-        } else if(doc_value.value_link !== null) {
-            return {
-                docValue: doc_value.value_link,
-                type: 'link'
-            };
-        } else if(doc_value.value_price !== null) {
-            return {
-                docValue: doc_value.value_price,
-                type: 'price'
-            };
-        } else if(doc_value.value_number !== null) {
-            return  {
-                docValue: doc_value.value_number,
-                type: 'number'
-            };
-        } else if(doc_value.value_html !== null) {
-            return  {
-                docValue: doc_value.value_html,
-                type: 'html'
-            };
-        } else if(doc_value.value_date !== null) {
-            return {
-                docValue: doc_value.value_date,
-                type: 'date'
-            };
-        } else if(doc_value.value_date_time !== null) {
-            return {
-                docValue: doc_value.value_date_time,
-                type: 'date_time'
-            };
-        } else if(doc_value.value_image !== null) {
-            return {
-                docValue: doc_value.value_image,
-                type: 'image',
-                medias: doc_value.medias
-            };
-        } else if(doc_value.value_gallery !== null) {
-            return {
-                docValue: doc_value.value_gallery,
-                type: 'gallery',
-                medias: doc_value.medias
-            };
-        } else if(doc_value.value_carousel !== null) {
-            return {
-                docValue: doc_value.value_carousel,
-                type: 'carousel',
-                medias: doc_value.medias
-            };
-        } else if(doc_value.value_audio !== null) {
-            return {
-                docValue: doc_value.value_audio,
-                type: 'audio',
-                medias: doc_value.medias,
-                id: doc_value.id
-            };
+        if (doc_value !== undefined) {
+            if (doc_value.value_text !== null) {
+                return {
+                    docValue: doc_value.value_text,
+                    type: 'text'
+                };
+            } else if(doc_value.value_link !== null) {
+                return {
+                    docValue: doc_value.value_link,
+                    type: 'link'
+                };
+            } else if(doc_value.value_price !== null) {
+                return {
+                    docValue: doc_value.value_price,
+                    type: 'price'
+                };
+            } else if(doc_value.value_number !== null) {
+                return  {
+                    docValue: doc_value.value_number,
+                    type: 'number'
+                };
+            } else if(doc_value.value_html !== null) {
+                return  {
+                    docValue: doc_value.value_html,
+                    type: 'html'
+                };
+            } else if(doc_value.value_date !== null) {
+                return {
+                    docValue: doc_value.value_date,
+                    type: 'date'
+                };
+            } else if(doc_value.value_date_time !== null) {
+                return {
+                    docValue: doc_value.value_date_time,
+                    type: 'date_time'
+                };
+            } else if(doc_value.value_image !== null) {
+                return {
+                    docValue: doc_value.value_image,
+                    type: 'image',
+                    medias: doc_value.medias
+                };
+            } else if(doc_value.value_gallery !== null) {
+                return {
+                    docValue: doc_value.value_gallery,
+                    type: 'gallery',
+                    medias: doc_value.medias
+                };
+            } else if(doc_value.value_carousel !== null) {
+                return {
+                    docValue: doc_value.value_carousel,
+                    type: 'carousel',
+                    medias: doc_value.medias
+                };
+            } else if(doc_value.value_audio !== null) {
+                return {
+                    docValue: doc_value.value_audio,
+                    type: 'audio',
+                    medias: doc_value.medias,
+                    id: doc_value.id
+                };
+            }
         }
     },
     getPrice(docValues) {
