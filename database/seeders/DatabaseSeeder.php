78ccd2d3817141513e12c66290aa88bbb4e2b089
ElliotDeganai <34213433+ElliotDeganai@users.noreply.github.com>
2024-11-30 03:17:00 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            //RoleTableSeeder::class,
+            RoleTableSeeder::class,
             UserTableSeeder::class,
             //CategorySeeder::class,
             //DocumentationSeeder::class,
